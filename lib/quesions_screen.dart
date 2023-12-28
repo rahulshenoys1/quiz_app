812bb45b1bc7903310efbdfd6cbb410c0425bbe9
rahulshenoys1 <rahul.shenoy@exathought.com>
2023-12-28 16:24:04 +0530

    created quiz and quesions_screen files

---
+++ lib/quesions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class QuesionsScreen extends StatefulWidget {
+  @override
+  State<QuesionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuesionsScreen> {
+  @override
+  Widget build(context) {
+    return const Text('QuesionsScreen');
+  }
+}
