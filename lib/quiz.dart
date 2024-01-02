efb2af3b7bb2d9f9ac22c2dfcff1526390c046cb
rahulshenoys1 <rahul.shenoy@exathought.com>
2024-01-02 11:19:30 +0530

    additions of few lines of code

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:quiz_app/quesions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
-  const Quiz ({super.key});
+  const Quiz({super.key});
 
   @override
   State<Quiz> createState() {
@@ -12,22 +12,51 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  // Widget? activeScreen;
+
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  var activeScreen = 'startScreen';
+
+  void switchScreen() {
+    setState(() {
+      // activeScreen = QuesionsScreen();
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
+    // final screenWidget = activeScreen == 'start-screen'
+    //     ? StartScreen(switchScreen)
+    //     : QuesionsScreen();
+
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'quesions-screen') {
+      screenWidget = QuesionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 78, 13, 151),
-                  Color.fromARGB(255, 107, 15, 168),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            child: const StartScreen()),
+          ),
+          // child: activeScreen,
+          child: screenWidget,
+        ),
       ),
     );
   }
